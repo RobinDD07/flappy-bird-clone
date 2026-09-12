96bf0cf1713f490509697d8282317d7420edf060
RobinDD07 <robin.cnetic06@gmail.com>
2026-09-12 16:01:04 +0530

    added pipe generation

--- main.gd
+++ main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var pipe_scene: PackedScene
+
 var game_running: bool
 var game_over: bool
 
@@ -8,15 +10,19 @@ var score
 const scroll_speed: int = 4 
 var pipes: Array
 
-#const pipe_delay
-#const pipe_range
+const pipe_delay: int = 100
+const pipe_range: int = 200
 
 var screen_size : Vector2i #vector2i integers lega, whereas vector2 decimals leta hai
+var ground_height: int
 
 func new_game():
 	game_running = false
 	game_over = false
+	pipes.clear()
+	generate_pipes()
 	$bird.reset()
+	$PipeTimer.start()
 	
 func start_game():
 	game_running = true
@@ -26,7 +32,9 @@ func start_game():
 
 func _ready():
 	screen_size = get_window().size
+	ground_height = $ground.get_node("Sprite2D").texture.get_height()
 	new_game()
+	
 
 func _input(event):
 	if game_over == false:
@@ -43,4 +51,20 @@ func _process(delta):
 		scroll += scroll_speed
 		if scroll >= screen_size.x:
 			scroll = 0
-		$Ground.position.x = -scroll + 864
+		$ground.position.x = -scroll + 864
+		
+		for pipe in pipes:
+			pipe.position.x -= scroll_speed + delta
+
+
+
+func _on_pipe_timer_timeout() -> void:
+	generate_pipes()
+	
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + pipe_delay
+	pipe.position.y = (screen_size.y - ground_height)/2 + randi_range(-pipe_range, pipe_range)
+	#pipe.hit.connect(bird_hit)
+	add_child(pipe)
+	pipes.append(pipe)
